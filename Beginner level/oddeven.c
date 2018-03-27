2516ab890b364c194ec7a36ac63cc7228b2efce0
sowmyabharathi <37831119+sowmyabharathi@users.noreply.github.com>
2018-03-27 16:05:24 +0530

    Create oddeven.c

---
+++ Beginner level/oddeven.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x;
+x=int(input("Enter a value"))
+if (x%2==0):
+    print ("The Number is even")
+else:
+    print ("The Number is Odd")
+    }
