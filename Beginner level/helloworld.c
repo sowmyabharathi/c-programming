e53c9ae1bd52bfd5eee555e1d9b62e9102025ba3
sowmyabharathi <37831119+sowmyabharathi@users.noreply.github.com>
2018-03-27 14:52:38 +0530

    Create helloworld.c

---
+++ Beginner level/helloworld.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(void) {
+	n = float(input("Enter a number: "))
+if (1<n<=100000):
+   print("Positive number");
+elif n == 0:
+   print("Zero");
+else:
+   print("Negative number");// your code goes here
+	return 0;
+}
